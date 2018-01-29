e5c07ac475fbcd51124f702a506bb1556fb09ab6
andrewkjankowski <andrewkjankowski@gmail.com>
2018-01-29 16:07:02 -0500

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,3 +1,8 @@
 class Artist
   # code goes here
+  attr_accessor :name, :song
+
+  def initialize(artist_name)
+    @name = artist_name
+  end
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,17 @@
+require 'tempfile'
+
 class Song
   # code goes here
+  attr_accessor :title, :artist
+
+  def initialize
+
+  end
+
+  def slugify
+    file_prefix = @title.downcase.gsub(" ", "_")
+    file = Tempfile.new([file_prefix, ".txt"], "tmp")
+    file.write("#{@artist.name} - #{@title}")
+    file.close
+  end
 end
